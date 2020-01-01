cb88b9e9213e315cc850d09b1b3fb9c3afcaaf24
Santoshteknue <54737601+Santoshteknue@users.noreply.github.com>
2020-01-01 09:33:25 -0800

    Add files via upload

---
+++ app/Controller.js
@@ -0,0 +1,8 @@
+
+var app = angular.module('newApp');
+ 
+  app.controller('Controller', 
+  function($scope, $rootScope, $stateParams, $state, Service) {
+    $scope.user = $rootScope.userName;
+    
+  });
